d0f0535d274c1c8a315f5209c8ab84ba0785f76f
Kang Hyojun <hyojun@admire.kr>
2015-01-31 22:58:56 +0900

    add jsonify

--- soran/web/response.py
+++ soran/web/response.py
@@ -2,17 +2,48 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-from flask import jsonify
+from datetime import datetime, date
+from functools import singledispatch
 
-__all__ = 'ok', 'created',
+import json
+
+from flask import jsonify as flask_jsonify
+
+from ..user import User
+
+__all__ = 'ok', 'created', 'jsonify'
 
 def ok(message='', **kwargs):
     """Return 200 response.
     """
-    return jsonify(message=message, data=kwargs), 200
+    return flask_jsonify(message=message, data=kwargs), 200
 
 
 def created(message='', **kwargs):
     """Return 201 response.
     """
-    return jsonify(message=message, data=kwargs), 201
+    return flask_jsonify(message=message, data=kwargs), 201
+
+
+@singledispatch
+def jsonify(arg):
+    return json.dumps(arg)
+
+
+@jsonify.register(User)
+def _(arg):
+    o = {
+        'id': arg.id,
+        'username': arg.username,
+        'password': arg.password,
+        'service': arg.service,
+        'created_at': jsonify(arg.created_at),
+        'modified_at': jsonify(arg.modified_at)
+    }
+    return jsonify(o)
+
+
+@jsonify.register(datetime)
+@jsonify.register(date)
+def _(arg):
+    return arg.strftime('%Y-%m-%dT%H:%M:%S')
