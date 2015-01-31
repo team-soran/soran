34599abea0d61a0a7ef91e420d30be79dbb0975e
Kang Hyojun <hyojun@admire.kr>
2015-02-01 00:01:18 +0900

    Rename jsonify -> jsonable

--- soran/web/response.py
+++ soran/web/response.py
@@ -7,43 +7,63 @@ from functools import singledispatch
 
 import json
 
-from flask import jsonify as flask_jsonify
+from flask import jsonify
 
 from ..user import User
 
-__all__ = 'ok', 'created', 'jsonify'
+__all__ = 'ok', 'created', 'jsonable'
 
-def ok(message='', **kwargs):
+def ok(message='', depth=2, **kwargs):
     """Return 200 response.
+
+    :param str message: a message
+    :param int depth: parse depth of data.
+    :return: a response that contain a json.
     """
-    return flask_jsonify(message=message, data=kwargs), 200
+    return jsonify(message=message, data=jsonable(kwargs, depth)), 200
 
 
-def created(message='', **kwargs):
+def created(message='', depth=2, **kwargs):
     """Return 201 response.
+
+    :param str message: a message
+    :param int depth: parse depth of data.
+    :return: a response that contain a json.
     """
-    return flask_jsonify(message=message, data=kwargs), 201
+    return jsonify(message=message, data=jsonable(kwargs, depth)), 201
 
 
 @singledispatch
-def jsonify(arg):
-    return json.dumps(arg)
+def jsonable(arg, depth=1):
+    return arg
 
 
-@jsonify.register(User)
-def _(arg):
-    o = {
-        'id': arg.id,
-        'username': arg.username,
-        'password': arg.password,
-        'service': arg.service,
-        'created_at': jsonify(arg.created_at),
-        'modified_at': jsonify(arg.modified_at)
-    }
-    return jsonify(o)
+@jsonable.register(dict)
+def _(arg, depth=1):
+    if depth >= 1:
+        depth -= 1
+        for k, v in arg.copy().items():
+            arg[k] = jsonable(v, depth)
+        return arg
+    return {}
 
 
-@jsonify.register(datetime)
-@jsonify.register(date)
-def _(arg):
+@jsonable.register(User)
+def _(arg, depth=1):
+    o = {'id': arg.id}
+    if depth >= 1:
+        depth -= 1
+        o.update({
+            'username': arg.username,
+            'password': arg.password,
+            'service': arg.service,
+            'created_at': jsonable(arg.created_at, depth),
+            'modified_at': jsonable(arg.modified_at, depth)
+        })
+    return o
+
+
+@jsonable.register(datetime)
+@jsonable.register(date)
+def _(arg, depth=1):
     return arg.strftime('%Y-%m-%dT%H:%M:%S')
