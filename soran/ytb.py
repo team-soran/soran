1ec137eecb3d67123c22ae042955129929e5e439
Kang Hyojun <hyojun@admire.kr>
2015-03-17 02:40:48 +0900

    Fill out real data to Youtube.metdata

--- soran/ytb.py
+++ soran/ytb.py
@@ -13,13 +13,26 @@ class Youtube:
         self.id_ = id_
         self.metadata = {}
 
-    def request_metdata(self):
+    def request_metadata(self):
         response = urlopen(self.to_url)
         if response.status != 200:
             raise Exception('status_code: {}, url: {}'.format(
                 response.status, self.to_url))
-        html = html5lib.parse(response.readall())
+        html = html5lib.parse(response.readall().decode('utf-8'),
+                              namespaceHTMLElements=False)
+        xpath_builder = "./head//meta[@{attr}='{value}']".format
+        query = [
+            ('property', 'og:title'),
+            ('property', 'og:image'),
+            ('name', 'keywords')
+        ]
+        for attr, value in query:
+            elem = html.findall(xpath_builder(attr=attr, value=value))
+            if elem:
+                self.metadata[value] = elem[0].get('content')
+            self.metadata.setdefault(value, None)
+        return self.metadata
 
     @property
     def to_url(self):
-        return _url + self.id_
+        return self._url + self.id_
