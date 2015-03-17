9a2f6942a185b43b6c4ff5e63c343483fdd00430
jihyun <noblea1117@gmail.com>
2015-03-18 02:49:12 +0900

    add artist model, unittest #13

---
+++ tests/artist_test.py
@@ -0,0 +1,21 @@
+from soran.artist import Artist
+
+
+def test_create_user(f_session):
+    name = 'aaa'
+    service = 'naver'
+    artist = Artist(name=name, service=service)
+    f_session.add(artist)
+    f_session.commit()
+    find_artist = f_session.query(Artist)\
+        .filter(Artist.name == name)\
+        .first()
+    assert find_artist
+    assert hasattr(find_artist, 'id')
+    assert find_artist.id
+    assert hasattr(find_artist, 'created_at')
+    assert find_artist.created_at
+    assert hasattr(find_artist, 'updated_at')
+    assert find_artist.updated_at
+    assert name == find_artist.name
+    assert service == find_artist.service
