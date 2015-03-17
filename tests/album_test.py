c07e5c2ba98928fa1ff66a858dab304e06937ed5
jihyun <noblea1117@gmail.com>
2015-03-18 02:42:12 +0900

    add unit test #12

---
+++ tests/album_test.py
@@ -0,0 +1,21 @@
+from soran.album import Album
+
+
+def test_create_user(f_session):
+    name = 'aaa'
+    service = 'naver'
+    album = Album(name=name, service=service)
+    f_session.add(album)
+    f_session.commit()
+    find_album = f_session.query(Album)\
+        .filter(Album.name == name)\
+        .first()
+    assert find_album
+    assert hasattr(find_album, 'id')
+    assert find_album.id
+    assert hasattr(find_album, 'created_at')
+    assert find_album.created_at
+    assert hasattr(find_album, 'updated_at')
+    assert find_album.updated_at
+    assert name == find_album.name
+    assert service == find_album.service
